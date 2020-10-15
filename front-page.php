0e58782b57f487936ba4781678550d3acd426c19
Rootdiv <55033154+Rootdiv@users.noreply.github.com>
2020-10-15 17:35:26 +0300

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -16,19 +16,19 @@
                         setup_postdata( $post );
                         ?>
                         <!-- Выводим записи -->
-                <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="post-thumb" />
+                <img src="<?php the_post_thumbnail_url(); ?>" alt="<?=the_title()?>" class="post-thumb" />
                 <?php $author_id = get_the_author_meta('ID'); ?>
-                <a href="<?php echo get_author_posts_url($author_id); ?>" class="author">
-                    <img src="<?php echo get_avatar_url($author_id); ?>" alt="<?php echo get_the_author(); ?>" class="avatar" />
+                <a href="<?=get_author_posts_url($author_id)?>" class="author">
+                    <img src="<?=get_avatar_url($author_id)?>" alt="<?=get_the_author()?>" class="avatar" />
                     <div class="author-bio">
-                        <span class="author-name"><?php echo get_the_author(); ?></span>
+                        <span class="author-name"><?=get_the_author()?></span>
                         <span class="author-rank">Должность</span>
                     </div>
                 </a>
                 <div class="post-text">
                     <?php the_category(); ?>
-                    <h2 class="post-title"><?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?></h2>
-                    <a href="<?php echo get_the_permalink(); ?>" class="more">Читать далее</a>
+                    <h2 class="post-title"><?=mb_strimwidth(get_the_title(), 0, 60, '...')?></h2>
+                    <a href="<?=get_the_permalink()?>" class="more">Читать далее</a>
                 </div>
                 <?php }
                 } else {
@@ -57,8 +57,8 @@
                             <!-- Выводим записи -->
                     <li class="post">
                         <?php the_category(); ?>
-                        <a class="post-permalink" href="<?php echo get_the_permalink(); ?>">
-                            <h4 class="post-title"><?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?></h4>
+                        <a class="post-permalink" href="<?=get_the_permalink()?>">
+                            <h4 class="post-title"><?=mb_strimwidth(get_the_title(), 0, 60, '...')?></h4>
                         </a>
                     </li>
                     <?php }
@@ -91,10 +91,10 @@
         ?>
         <!-- Выводим записи -->
         <li class="article-item">
-            <a class="article-permalink" href="<?php echo get_the_permalink(); ?>">
-                <h4 class="article-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...'); ?></h4>
+            <a class="article-permalink" href="<?=get_the_permalink()?>">
+                <h4 class="article-title"><?=mb_strimwidth(get_the_title(), 0, 50, '...')?></h4>
             </a>
-            <img width="65" height="65" src="<?php echo get_the_post_thumbnail_url( null, 'thumbnail'); ?>" alt="<?php the_title(); ?>">
+            <img width="65" height="65" src="<?=get_the_post_thumbnail_url( null, 'thumbnail')?>" alt="<?php the_title(); ?>">
         </li>
         <?php }
         } else {
