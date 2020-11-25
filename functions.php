6687239657120b10b1a9fab4e6be708c5a6ffc60
Rootdiv <rootdiv@mail.ru>
2020-11-25 18:54:05 +0300

    Cвой тип записей

--- functions.php
+++ functions.php
@@ -21,6 +21,48 @@ if ( ! function_exists( 'universal_theme_setup' ) ) :
       'header_menu' => 'Menu in header',
       'footer_menu' => 'Menu in footer'
     ] );
+
+    add_action( 'init', 'register_post_types' );
+    function register_post_types(){
+      register_post_type( 'lesson', [
+        'label'  => null,
+        'labels' => [
+          'name'               => 'Уроки', // основное название для типа записи
+          'singular_name'      => 'Урок', // название для одной записи этого типа
+          'add_new'            => 'Добавить урок', // для добавления новой записи
+          'add_new_item'       => 'Добавление урока', // заголовка у вновь создаваемой записи в админ-панели.
+          'edit_item'          => 'Редактирование урока', // для редактирования типа записи
+          'new_item'           => 'Новый урок', // текст новой записи
+          'view_item'          => 'Смотреть уроки', // для просмотра записи этого типа.
+          'search_items'       => 'Искать уроки', // для поиска по этим типам записи
+          'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+          'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+          'parent_item_colon'  => '', // для родителей (у древовидных типов)
+          'menu_name'          => 'Уроки', // название меню
+        ],
+        'description'         => 'Раздел с видеоуроками',
+        'public'              => true,
+        // 'publicly_queryable'  => null, // зависит от public
+        // 'exclude_from_search' => null, // зависит от public
+        // 'show_ui'             => null, // зависит от public
+        // 'show_in_nav_menus'   => null, // зависит от public
+        'show_in_menu'        => true, // показывать ли в меню адмнки
+        // 'show_in_admin_bar'   => null, // зависит от show_in_menu
+        'show_in_rest'        => true, // добавить в REST API. C WP 4.7
+        'rest_base'           => null, // $post_type. C WP 4.7
+        'menu_position'       => 5,
+        'menu_icon'           => 'dashicons-welcome-learn-more',
+        'capability_type'     => 'post',
+        //'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+        //'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+        'hierarchical'        => false,
+        'supports'            => ['title', 'editor', 'thumbnail', 'custom-fields'], #'author','excerpt','trackbacks','comments','revisions','page-attributes','post-formats'
+        'taxonomies'          => [],
+        'has_archive'         => true,
+        'rewrite'             => true,
+        'query_var'           => true,
+      ] );
+    }
   }
 endif;
 add_action( 'after_setup_theme', 'universal_theme_setup' );
---
+++ template-parts/content-lesson.php
@@ -0,0 +1,104 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <!-- Шапка поста -->
+  <header class="entry-header <?=get_post_type()?>-header" style="background: linear-gradient(0deg, rgba(38, 45, 51, 0.75), rgba(38, 45, 51, 0.75));">
+		<div class="container">
+      <div class="post-header-wrapper">
+        <div class="post-header-nav">
+          <?php foreach (get_the_category() as $category) {
+            printf(
+              '<a href="%s" class="category-link %s">%s</a>',
+              esc_url(get_category_link($category)),
+              esc_html($category->slug),
+              esc_html($category->name),
+            );
+          }?>
+        </div>
+        <div class="video">
+          <?php $video = get_field('video_link');
+            if(stristr($video, 'youtu')){
+              $link = explode('/', $video);
+              $video_link = end($link);
+              echo '<iframe width="100%" height="450" src="https://www.youtube.com/embed/'.$video_link.'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+            }elseif(stristr($video, 'youtube')){
+              $link = explode('?v=', $video);
+              $video_link = end($link);
+              echo '<iframe width="100%" height="450" src="https://www.youtube.com/embed/'.$video_link.'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+            }elseif(stristr($video, 'vimeo')){
+              $link = explode('/', $video);
+              $video_link = end($link); 
+              echo '<iframe src="https://player.vimeo.com/video/'.$video_link.'" width="100%" height="450" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>';
+            }
+          ?>
+        </div>
+        <!-- /.post-header-nav -->
+        <div class="lesson-header">
+          <div class="lesson-header-title-wrapper">
+            <?php //Проверяем точно ли мы на странице поста
+            if ( is_singular() ) :
+              the_title( '<h1 class="lesson-header-title">', '</h1>' );
+            else :
+              the_title( '<h2 class="lesson-header-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+            endif;?>
+          </div>
+        </div>
+        <div class="post-header-info">
+          <div class="post-header-date">
+            <svg width="14" height="14" class="icon clocks-icon">
+              <use xlink:href="<?=get_template_directory_uri()?>/assets/images/sprite.svg#clock"></use>
+            </svg>
+            <span><?php the_time('j F, G:i')?></span>
+          </div>
+        </div>
+        <!-- /.post-header-info -->
+      </div>
+      <!-- /.post-header-wrapper -->
+    </div>
+	</header><!-- /Шапка поста -->
+  <div class="container">
+    <!-- Содержимое поста -->
+    <div class="post-content">
+      <?php //Содержимое поста
+      the_content(
+        sprintf(
+          wp_kses(
+            /* translators: %s: Name of current post. Only visible to screen readers */
+            __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'universal-theme' ),
+            array(
+              'span' => array(
+                'class' => array(),
+              ),
+            )
+          ),
+          wp_kses_post( get_the_title() )
+        )
+      );
+
+      wp_link_pages(
+        array(
+          'before' => '<div class="page-links">' . esc_html__( 'Страницы:', 'universal-theme' ),
+          'after'  => '</div>',
+        )
+      );?>
+    </div><!-- /Содержимое поста -->
+    <!-- Подвал поста-->
+    <footer class="entry-footer">
+      <?php $tags_list = get_the_tag_list( '', esc_html_x( '', 'list item separator', 'universal-theme' ) );
+        if ( $tags_list ) {
+          //Список тегов
+          printf( '<span class="tags-links">' . esc_html__( '%1$s', 'universal-theme' ) . '</span>', $tags_list );
+        } 
+        //Поделится в соцсетях
+        meks_ess_share();
+      ?>
+    </footer><!-- /Подвал поста-->
+  </div>
+  <!-- /.container -->
+  <div class="sidebar-post">
+    <div class="container">
+      <!-- Подключаем сайдбар -->
+      <?php //get_sidebar('articles') ?>
+    </div>
+    <!-- /.container -->
+  </div>
+  <!-- /.sidebar-post -->
+</article>
