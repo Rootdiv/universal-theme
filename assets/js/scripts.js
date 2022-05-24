2ed42bfd17a98b28a4cfc43e76dac24c0df664d2
Rootdiv <rootdiv@mail.ru>
2022-05-25 00:46:47 +0300

    fix: up arrow and code style

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,29 @@
 'use strict';
 
 //Плавная прокрутка страницы вверх
-window.addEventListener('scroll', function() {
-  const scrollHeight = Math.round(window.scrollY);
-  if (scrollHeight < 300) document.querySelector('.go-top').style.opacity = '0';
-  else document.getElementsByClassName('go-top')[0].style.opacity = '1';
-});
+const arrowTop = document.querySelector('.go-top');
+if (arrowTop) {
+  window.addEventListener('scroll', function() {
+    const scrollHeight = Math.round(window.scrollY);
+    if (scrollHeight < 300) {
+      arrowTop.style.opacity = '0';
+    } else {
+      arrowTop.style.opacity = '1';
+    }
+  });
 
-function backToTop() {
-  if (window.pageYOffset > 0) {
-    window.scrollBy(0, -10);
-    setTimeout(backToTop, 0);
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -10);
+      setTimeout(backToTop, 0);
+    }
   }
+  arrowTop.addEventListener('click', function() {
+    backToTop();
+  });
 }
-document.querySelector('.go-top').addEventListener('click', function() {
-  backToTop();
-});
 
-const mySwiper = new Swiper('.swiper-container', { //jshint ignore:line
+const mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
 
@@ -31,28 +37,28 @@ const mySwiper = new Swiper('.swiper-container', { //jshint ignore:line
   },
 });
 
-jQuery(function ($) {
+jQuery(function($) {
   const menuToggle = $('.header-menu-toggle');
-  menuToggle.on('click', function (event) {
+  menuToggle.on('click', function(event) {
     event.preventDefault();
     $('.header-nav').slideToggle(200);
   });
 
   const contactsForm = $('.contacts-form');
 
-  contactsForm.on('submit', function (event) {
+  contactsForm.on('submit', function(event) {
     event.preventDefault();
     let formData = new FormData(this);
     formData.append('action', 'contacts_form');
     $.ajax({
-      type: "POST",
+      type: 'POST',
       url: adminAjax.url, //jshint ignore:line
       data: formData,
       contentType: false,
       processData: false,
-      success: function (response) {
+      success: function(response) {
         console.log('Ответ сервера: ' + response);
-      }
+      },
     });
   });
 });
